d78ef6e0a8b4cd217f2ed7ae59778562b22ad3ea
Faith <127827447+Remii1@users.noreply.github.com>
2023-03-22 16:19:49 +0300

    Add files via upload

---
+++ primer.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number:"))
+
+if num >1:
+    for 1 in range(2,int(num/2)+1):
+        if (num%1)==0:
+            print(num,"not")
+            break
+        else:
+            print(num,"it is")
+        else:
+            print(num,"not")
+
