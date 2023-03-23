aecdea82218583547fbb5d94a5c39d7782be5de1
Faith <127827447+Remii1@users.noreply.github.com>
2023-03-23 22:18:04 +0300

    Add files via upload

--- primer.py
+++ primer.py
@@ -1,12 +1,26 @@
-num = int(input("Enter a number:"))
+import math
 
-if num >1:
-    for 1 in range(2,int(num/2)+1):
-        if (num%1)==0:
-            print(num,"not")
+# Accept user input as a float number
+num = float(input("Enter a number: "))
+
+# Convert the float number to an integer
+num = int(num)
+
+num_prime = False
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # Check for factors up to the square root of the number
+    for i in range(2, int(math.sqrt(num))+1):
+        if (num % i) == 0:
+            # If factor is found, set num_prime to True
+            num_prime = True
+            # Break out of loop
             break
-        else:
-            print(num,"it is")
-        else:
-            print(num,"not")
 
+    # Check if num_prime is True
+    if num_prime:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
