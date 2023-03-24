0ad935e1cde001ae3a447cce7f5fdd50ffc9a26c
Faith <127827447+Remii1@users.noreply.github.com>
2023-03-24 16:34:35 +0300

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,17 @@
+furniture = ["table", "chair", "stool"]
+
+for x in furniture:
+
+    print(x)
+
+#adding an item in an array
+furniture.append("bed")
+print(furniture)
+
+#removing an item in an array
+furniture.remove("stool")
+print(furniture)
+
+#length of an array
+x = len(furniture)
+print(x)
